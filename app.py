26f1f58113edb7370263bc23ab28dc8791e593f4
Kosuke Shigematsu <146371194+kosukesh34@users.noreply.github.com>
2025-02-23 22:35:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -600,5 +600,6 @@ def index():
         return send_file('brawl_stars.png', as_attachment=True)
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Renderが指定するPORTを取得（デフォルト10000）
+    app.run(host="0.0.0.0", port=port, debug=True)
